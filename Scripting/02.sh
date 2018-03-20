1fe819ec00c78e54eb54a88188fd757a3f8ffed2
root <root@debian-s-1vcpu-1gb-lon1-01.localdomain>
2018-03-20 12:25:15 +0000

    add

--- Scripting/02.sh
+++ Scripting/02.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 exec >> /var/log/update_script.log
 apt-get update -y &
-exec apt-get install -y
+exec apt-get upgrade -y
 
-
-
-# Pour Cron
-# apt-get install cron
-# crontab -e 
-# chmod 777 02.sh
-# mettre chmod /root/
-# * 4 * * * * /bin/sh /root/02.sh
